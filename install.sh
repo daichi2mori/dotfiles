cec362614c08a8fa5246bd793772a6b513f1b36d
f0reth <65765140+f0reth@users.noreply.github.com>
2026-06-28 17:30:03 +0900

    chore: インストール方法更新

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ chsh -s "$(which zsh)"
 
 # Starship のインストール
 log_info "Starship をインストール中..."
-curl -sS https://starship.rs/install.sh | sh -s -- -y
+sudo apt install -y starship
 
 # Sheldon のインストール
 log_info "Sheldon をインストール中..."
@@ -44,11 +44,6 @@ curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
 
 # eza のインストール
 log_info "eza をインストール中..."
-sudo mkdir -p /etc/apt/keyrings
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-sudo apt update
 sudo apt install -y eza
 
 # ripgrep のインストール
